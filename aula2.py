ad3d98bceb0b8299499637c12866102a22b94e7b
PauloStraforini <paulostraforini@gmail.com>
2025-01-13 16:35:28 -0300

    📦 Add Python

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# r = Return 
+# n = Line feed  
+
+# sep = (separador), end = (finalizador)
+
+print (12, 34, 28, 63, 100, sep=' ,', end='##')
+print (14, 63, 120, sep=' ,', end='__')
+print (28, 32, sep=' ,', end='\r\n')
